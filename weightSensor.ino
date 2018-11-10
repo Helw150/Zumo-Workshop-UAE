0444979de0917365935802a441b189b796a4ac6a
William Held <Wbh230@nyu.edu>
2018-11-10 11:05:19 +0400

    Create weightSensor.ino

---
+++ weightSensor.ino
@@ -0,0 +1,33 @@
+
+const int FSR_PIN = A0; // Pin connected to FSR/resistor divider
+
+// Measure the voltage at 5V and resistance of your 3.3k resistor, and enter
+// their value's below:
+const float VCC = 4.98; // Measured voltage of Ardunio 5V line
+const float R_DIV = 3230.0; // Measured resistance of 3.3k resistor
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(FSR_PIN, INPUT);
+}
+
+void loop() 
+{
+  int fsrADC = analogRead(FSR_PIN);
+  // If the FSR has no pressure, the resistance will be
+  // near infinite. So the voltage should be near 0.
+  if (fsrADC != 0) // If the analog reading is non-zero
+  {
+    // Use ADC reading to calculate voltage:
+    float fsrV = fsrADC * VCC / 1023.0;
+    // Use voltage and static resistor value to 
+    // calculate FSR resistance:
+    float fsrR = R_DIV * (VCC / fsrV - 1.0);
+    Serial.println("Resistance: " + String(fsrR) + " ohms");
+  }
+  else
+  {
+    // No pressure detected
+  }
+}
